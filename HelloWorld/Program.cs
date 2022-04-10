81852ea6c07ab8b0b8a45384f02006a0d2f32835
klawiat <76530186+klawiat@users.noreply.github.com>
2022-04-10 23:32:10 +0300

    First commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Console.WriteLine("Hi!\nWhat is your name?");
+string? Name = Console.ReadLine();
+Console.WriteLine($"Nise to meeet you {Name}");*/
+Console.WriteLine("Hello World!");
