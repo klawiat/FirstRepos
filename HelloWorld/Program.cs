4dc7c2376cc5ea2affda1c7fbdca5615f0f3cdd8
klawiat <kirill.britsky@yandex.ru>
2022-04-12 01:01:08 +0300

    add new and fix file, fix bags and new features

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,5 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-/*Console.WriteLine("Hi!\nWhat is your name?");
+Console.WriteLine("Hi!\nWhat is your name?");
 string? Name = Console.ReadLine();
-Console.WriteLine($"Nise to meeet you {Name}");*/
-Console.WriteLine("Hello World!");
+Console.WriteLine($"Nise to meet you {Name}");
