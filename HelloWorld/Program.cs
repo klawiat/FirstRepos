49b3e7a99ae6ad72e4ecbfd5d2e5514121e8b51e
klawiat <kirill.britsky@yandex.ru>
2022-04-16 21:47:38 +0300

    run of admin

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hi!\nWhat is your name?");
-string? Name = Console.ReadLine();
-Console.WriteLine($"Nise to meet you {Name}");
+﻿/*Console.Write("Введите число:");
+int FirstNum = Convert.ToInt32(Console.ReadLine());
+if (FirstNum > 5 && FirstNum < 10)
+    Console.WriteLine("Число больше 5 и меньше 10");
+else Console.WriteLine("Неизвестное число");*/
+using System.Security.Principal;
+using System.IO;
+
+bool isElevated;
+using (WindowsIdentity identity = WindowsIdentity.GetCurrent())
+{
+    WindowsPrincipal principal = new WindowsPrincipal(identity);
+    isElevated = principal.IsInRole(WindowsBuiltInRole.Administrator);
+    string path = Path.GetFullPath("HelloWorld.exe");
+    if (isElevated == true)
+    {
+        Console.WriteLine("Программа работает от имени администратора");
+        Console.ReadLine();
+    }
+    else
+    {
+        Console.WriteLine("Программа работает без прав администратора");
+        System.Diagnostics.Process.Start("cmd.exe", "/C powershell start-process " + path + " -verb runas");
+
+    }
+    //Console.WriteLine(path);
+    //string command = "ping google.com -t";
+    //System.Diagnostics.Process.Start("cmd.exe", "/C " + command);
+
+    //Console.ReadLine();
+}
